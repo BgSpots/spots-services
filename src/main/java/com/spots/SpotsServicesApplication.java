6002441bd12a88369834da358208d3f05c5601ee
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-06-21 18:03:10 +0300

    feat: initial setup

---
+++ src/main/java/com/spots/SpotsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.spots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotsServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotsServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spots/SpotsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotsServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

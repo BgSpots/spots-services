839d82745123dd68936fb3f69f112de392d31a69
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-06-22 09:49:12 +0300

    test the new branch

--- src/test/java/com/spots/SpotsServicesApplicationTests.java
+++ src/test/java/com/spots/SpotsServicesApplicationTests.java
@@ -9,6 +9,8 @@ class SpotsServicesApplicationTests {
     @Test
     void contextLoads() {
 
-        for (int i = 0; i < 10; i++) {}
+        for (int i = 0; i < 10; i++) {
+            System.out.println("test");
+        }
     }
 }

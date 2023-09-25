e6ef65180295fb639c691ff411bb2f1620ac5e26
Yasen <91064048+yasenyankov31@users.noreply.github.com>
2023-09-25 16:58:51 +0300

    Automated data insertion for mongo db (#19)

--- src/main/java/com/spots/SpotsServicesApplication.java
+++ src/main/java/com/spots/SpotsServicesApplication.java
@@ -1,14 +1,18 @@
 package com.spots;
 
+import com.spots.dataInserter.DataInserter;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @OpenAPIDefinition
 public class SpotsServicesApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(SpotsServicesApplication.class, args);
+        DataInserter.addData();
     }
 }

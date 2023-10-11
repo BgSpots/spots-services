a5348b86ba1850eb2dcd1644fb885ac3d629816a
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-10-11 11:49:24 +0300

    feat: add first 10 spots

--- scripts/insert_data.py
+++ scripts/insert_data.py
@@ -2,19 +2,29 @@ import json
 from pymongo import MongoClient
 
 # MongoDB connection details
-mongo_host = "localhost"  # Replace with your MongoDB server host
-mongo_port = 27017        # Replace with your MongoDB server port
+mongo_host = "localhost"
+mongo_port = 27017
 
 # Connect to MongoDB
 client = MongoClient(mongo_host, mongo_port)
-db = client["spots"]  # Replace with your database name
-collection = db["spots"]  # Replace with your collection name
+db = client["spots"]
+spotsCollection= db["spots"]
+sequenceCollection= db["database_sequences"]
 
-# Read data from JSON file
-with open("data.json", "r") as file:
-  data = json.load(file)
+# Function to insert data from a JSON file into MongoDB
+def insert_data_from_json(json_file, collection):
+  with open(json_file, "r") as file:
+    data = json.load(file)
 
-# Insert data into MongoDB
-inserted_ids = collection.insert_many(data)
+  inserted_ids = collection.insert_many(data)
+  return len(inserted_ids.inserted_ids)
 
-print(f"Inserted {len(inserted_ids.inserted_ids)} documents into MongoDB")
+# Insert data from "spots.json"
+spots_inserted_count = insert_data_from_json("spots.json", spotsCollection)
+
+# Insert data from "squence.json"
+squence_inserted_count = insert_data_from_json("sequence.json", sequenceCollection)
+
+# Print the number of documents inserted from each file
+print(f"Inserted {spots_inserted_count} documents from 'spots.json' into MongoDB")
+print(f"Inserted {squence_inserted_count} documents from 'squence.json' into MongoDB")
